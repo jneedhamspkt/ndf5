db1bc5dc770b0933d53533e798c7544d17e1f0a1
Justin Needham <jneedhamspkt@gmail.com>
2018-02-12 20:10:43 -0500

    Initialize nDF5 project

---
+++ ndf5/ndf5.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace ndf5.tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Pass("Woot");
+        }
+    }
+}
