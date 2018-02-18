0cc92fe007350629ca78372b04067ca0b09d8e0b
Justin Needham <jneedhamspkt@gmail.com>
2018-02-18 00:39:02 -0500

    Adding File Info test

--- ndf5.tests/Loading.cs
+++ ndf5.tests/Loading.cs
@@ -34,5 +34,37 @@ namespace ndf5.tests
                 Hdf5File.Open(fTestStream).Dispose();
             }
         }
+
+        [Test, TestCaseSource(nameof(AllExampleFiles))]
+        public void Test_FileInfo_Loading(string aFile)
+        {
+            Assembly
+                fAssembly = Assembly.GetExecutingAssembly();
+
+            using (Stream fSource = fAssembly.GetManifestResourceStream(aFile))
+            {
+                //Write the File to a Temporary File and use FileInfo Arguments
+                string
+                fTestFileName = $"{Guid.NewGuid()}.h5";
+                using(FileStream fTest = new FileStream(
+                    fTestFileName,
+                    FileMode.CreateNew,
+                    FileAccess.ReadWrite, 
+                    FileShare.ReadWrite,
+                    64, 
+                    FileOptions.DeleteOnClose ))
+                {
+                    int 
+                        fLength = (int)fSource.Length;
+                    byte[]
+                        fFileBytes = new byte[fLength];
+                    fSource.Read(fFileBytes, 0, fLength);
+                    fTest.Write(fFileBytes, 0, fLength);
+                    fTest.Flush();
+
+                    Hdf5File.Open(new FileInfo(fTestFileName)).Dispose();
+                }
+            }
+        }
     }
 }
