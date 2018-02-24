2101c4ec79044dd012217899e106a015ff6b1bb9
Justin Needham <jneedhamspkt@gmail.com>
2018-02-24 17:08:26 -0500

    Added som test vectors from Lookup3.c's Driver5 function

--- ndf5.tests/Checksums/Lookup3.cs
+++ ndf5.tests/Checksums/Lookup3.cs
@@ -51,5 +51,40 @@ namespace ndf5.tests.Checksums
                     "Incorrect Hash");
             }
         }
+
+        [Test]
+        [TestOf(typeof(Test))]
+        public static void Confirm_driver5_from_original()
+        {
+            //uint32_t b, c;
+            //b = 0, c = 0, hashlittle2("", 0, &c, &b);
+            //printf("hash is %.8lx %.8lx\n", c, b);   /* deadbeef deadbeef */
+            Assert.That(
+                Test.ComputeHash(Encoding.ASCII.GetBytes(""), 0x0),
+                    Is.EqualTo(0xdeadbeef),
+                    "Incorrect Hash");
+
+            //b = 0xdeadbeef, c = 0, hashlittle2("", 0, &c, &b);
+            //printf("hash is %.8lx %.8lx\n", c, b);   /* bd5b7dde deadbeef */
+            Assert.That(
+                Test.ComputeHash(Encoding.ASCII.GetBytes(""), 0xdeadbeef),
+                     Is.EqualTo(0xbd5b7dde),
+                    "Incorrect Hash");
+            
+
+            //c = hashlittle("Four score and seven years ago", 30, 0);
+            //printf("hash is %.8lx\n", c);   /* 17770551 */
+            Assert.That(
+                Test.ComputeHash(Encoding.ASCII.GetBytes("Four score and seven years ago")),
+                    Is.EqualTo(0x17770551),
+                    "Incorrect Hash");
+
+            //c = hashlittle("Four score and seven years ago", 30, 1);
+            //printf("hash is %.8lx\n", c);   /* cd628161 */
+            Assert.That(
+                Test.ComputeHash(Encoding.ASCII.GetBytes("Four score and seven years ago"), 1),
+                    Is.EqualTo(0xcd628161),
+                    "Incorrect Hash");
+        }
     }
 }
