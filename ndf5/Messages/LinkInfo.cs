047731f19e9fe7ea4bb38d0d335f55c24582a9b1
Justin Needham <jneedhamspkt@gmail.com>
2018-03-13 07:31:26 -0400

    Link Info WIP

---
+++ ndf5/Messages/LinkInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ndf5.Streams;
+
+namespace ndf5.Messages
+{
+    public class LinkInfo : Message
+    {
+        const byte
+            mcCurrentVersion = 0;
+
+        [Flags]
+        enum Flags
+        {
+            None = 0,
+            CreationOrderTracked = 1,
+            CreationOrderIndexed = 2
+        }
+
+        public LinkInfo() : base(MessageType.LinkInfo)
+        {
+        }
+
+        internal static Message Read(
+            Hdf5Reader aReader,
+            long? aLocalMessageSize,
+            out long aBytes)
+        {
+            int 
+                fReadBytes = 4; //Always Read atleast 4
+            byte 
+                fVersion = (byte)aReader.ReadByte(),
+                fFlags = (byte)aReader.ReadByte();
+            aReader.ReadUInt16(); //Reserved
+
+            if (fVersion != mcCurrentVersion)
+                throw new Exceptions.UnknownMessageVersion<LinkInfo>(fVersion);
+
+            throw new NotImplementedException();
+        }
+    }
+}
