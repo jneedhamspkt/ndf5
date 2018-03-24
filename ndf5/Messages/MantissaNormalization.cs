b93c7ca3978ed58bab138b5550c52d61213e42e2
Justin Needham <jneedhamspkt@gmail.com>
2018-03-24 17:10:14 -0400

    Adding Floating Point Data Type (Not tested)

---
+++ ndf5/Messages/MantissaNormalization.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ndf5.Messages
+{
+    /// <summary>
+    /// Mantissa normalization, used byt <see cref="FloatingPointDataType"/>
+    /// </summary>
+    public enum MantissaNormalization : byte
+    {
+        /// <summary>
+        /// No normalization
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The most significant bit of the mantissa is always set (except for 0.0).
+        /// </summary>
+        MsbSetAndStored,
+
+        /// <summary>
+        /// The most significant bit of the mantissa is not stored, but is implied to be set.
+        /// </summary>
+        MsbSetNotStorred
+    }
+}
