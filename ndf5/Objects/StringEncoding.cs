82686c4cb3ed1c1749127471706f0354be8120e2
Justin Needham <jneedhamspkt@gmail.com>
2018-03-27 07:19:19 -0400

    Adding String base types

---
+++ ndf5/Objects/StringEncoding.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+namespace ndf5.Objects
+{
+    /// <summary>
+    /// Type of encidding used in an HDF5 string data type
+    /// </summary>
+    public enum StringEncoding : byte
+    {
+        ASCII = 0,
+        UTF8 = 1
+    }
+
+    public static class StringEncodingExtensions
+    {
+        /// <summary>
+        /// Gets the encoding implmntation for e specidified <see cref="StringEncoding"/>
+        /// </summary>
+        /// <returns>The encoding.</returns>
+        /// <param name="aValue">The StringEncoding to convert.</param>
+        public static Encoding GetEncoding(this StringEncoding aValue)
+        {
+            switch(aValue)
+            {
+                case StringEncoding.ASCII: return Encoding.ASCII;
+                case StringEncoding.UTF8: return Encoding.UTF8;
+                default:
+                    throw new System.ArgumentException($"Unknown Encoding: {aValue}");
+            }
+        }
+    }
+
+}
---
+++ ndf5/Objects/StringPadding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ndf5.Objects
+{
+    /// <summary>
+    /// Determines the type of padding to use for the string
+    /// </summary>
+    public enum StringPadding : byte
+    {
+        /// <summary>
+        /// A zero byte marks the end of the string and is guaranteed to be
+        /// present after converting a long string to a short string. When 
+        /// converting a short string to a long string the value is padded with 
+        /// additional null characters as necessary.
+        /// </summary>
+        NullTerminate = 0,
+        /// <summary>
+        /// Null Pad: Null characters are added to the end of the value during 
+        /// conversions from short values to long values but conversion in the 
+        /// opposite direction simply truncates the value.
+        /// </summary>
+        NullPad = 1,
+        /// <summary>
+        /// Space characters are added to the end of the value during 
+        /// conversions from short values to long values but conversion in the 
+        /// opposite direction simply truncates the value. This is the Fortran 
+        /// representation of the string.
+        /// </summary>
+        SpacePad = 2,
+    }
+}
