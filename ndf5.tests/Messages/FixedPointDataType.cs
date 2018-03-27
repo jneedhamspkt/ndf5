5a7b4abb4618d23e8ff345ab22b13eb975ec944d
Justin Needham <jneedhamspkt@gmail.com>
2018-03-27 06:11:14 -0400

    Add equality checks to Datatypes

--- ndf5.tests/Messages/FixedPointDataType.cs
+++ ndf5.tests/Messages/FixedPointDataType.cs
@@ -62,12 +62,49 @@ namespace ndf5.tests.Messages
                 {
                     long 
                         fReadBytes;
-                    uTest fResult = ndf5.Messages.Message.Read(
-                        fReader,
-                        uMessages.MessageType.Datatype,
-                        uMessages.MessageAttributeFlag.None,
-                        null,
-                        out fReadBytes) as uTest;
+                    uTest 
+                        fExpected = new uTest(
+                            aSize,
+                            aByteOrdering,
+                            aHighPaddingBit,
+                            aLowPaddingBit,
+                            aIsSigned,
+                            aBitOffset,
+                            aBitPrecision),
+                        fResult = ndf5.Messages.Message.Read(
+                            fReader,
+                            uMessages.MessageType.Datatype,
+                            uMessages.MessageAttributeFlag.None,
+                            null,
+                            out fReadBytes) as uTest;
+
+                    Assert.That(
+                        fResult,
+                        Is.EqualTo(fExpected),
+                        "Equality check failed");
+
+                    Assert.That(
+                        fResult,
+                        Is.Not.EqualTo(new uTest(
+                            aSize,
+                            aByteOrdering,
+                            aHighPaddingBit,
+                            aLowPaddingBit,
+                            aIsSigned,
+                            aBitOffset,
+                            (ushort)(aBitPrecision + 1))),
+                        "Inequality Check Failed");
+
+                    Assert.That(
+                        fResult,
+                        Is.Not.EqualTo(null),
+                        "Null Inequality Check Failed");
+
+                    Assert.That(
+                        fResult.GetHashCode(),
+                        Is.EqualTo(fExpected.GetHashCode()),
+                        "Hash Code Equality check failed");
+                    
                     Assert.That(
                         fResult,
                         Is.Not.Null,
